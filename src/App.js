b01bdc4379e9415fd8572b3432b0594747a196eb
Cong Van <congvan2405@gmail.com>
2023-11-14 17:10:23 +0700

    Build app UI

--- src/App.js
+++ src/App.js
@@ -2,8 +2,65 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="trello-amster">
+      <nav className="navbar app">Trello</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brain Storm</header>
+          <ul>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+            <li>First</li>
+            <li>Second</li>
+            <li>Third</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
