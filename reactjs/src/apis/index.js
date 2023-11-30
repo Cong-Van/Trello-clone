e573c6ffc7228d5b2df118143c3fb762ff55308e
Cong Van <congvan2405@gmail.com>
2023-12-01 05:34:36 +0700

    Add try catch for call API

--- reactjs/src/apis/index.js
+++ reactjs/src/apis/index.js
@@ -3,63 +3,103 @@ import { API_ROOT } from "../utils/constant";
 
 // Board
 export const fetchBoardDetailsAPI = async (boarId) => {
-  const response = await axios.get(`${API_ROOT}/boards/${boarId}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_ROOT}/boards/${boarId}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const updateBoardAPI = async (boardId, updateBoardData) => {
-  const response = await axios.put(
-    `${API_ROOT}/boards/${boardId}`,
-    updateBoardData
-  );
-  return response.data;
+  try {
+    const response = await axios.put(
+      `${API_ROOT}/boards/${boardId}`,
+      updateBoardData
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Column
 export const fetchColumnDetailsAPI = async (columnId) => {
-  const response = await axios.get(`${API_ROOT}/columns/${columnId}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_ROOT}/columns/${columnId}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const creatNewColumnAPI = async (newColumnData) => {
-  const response = await axios.post(`${API_ROOT}/columns`, newColumnData);
-  return response.data;
+  try {
+    const response = await axios.post(`${API_ROOT}/columns`, newColumnData);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const updateColumnAPI = async (columnId, updateColumnData) => {
-  console.log(columnId, updateColumnData);
-  const response = await axios.put(
-    `${API_ROOT}/columns/${columnId}`,
-    updateColumnData
-  );
-  console.log(response.data);
-  return response.data;
+  try {
+    console.log(columnId, updateColumnData);
+    const response = await axios.put(
+      `${API_ROOT}/columns/${columnId}`,
+      updateColumnData
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const deleteColumnAPI = (columnId) => {
-  axios.delete(`${API_ROOT}/columns/${columnId}`);
+  try {
+    axios.delete(`${API_ROOT}/columns/${columnId}`);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Card
 export const fetchCardDetailsAPI = async (cardId) => {
-  const response = await axios.get(`${API_ROOT}/cards/${cardId}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${API_ROOT}/cards/${cardId}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const creatNewCardAPI = async (newCardData) => {
-  const response = await axios.post(`${API_ROOT}/cards`, newCardData);
-  return response.data;
+  try {
+    const response = await axios.post(`${API_ROOT}/cards`, newCardData);
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const updateCardAPI = async (cardId, updateCardData) => {
-  console.log(cardId, updateCardData);
-  const response = await axios.put(
-    `${API_ROOT}/cards/${cardId}`,
-    updateCardData
-  );
-  return response.data;
+  try {
+    console.log(cardId, updateCardData);
+    const response = await axios.put(
+      `${API_ROOT}/cards/${cardId}`,
+      updateCardData
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const deleteCardAPI = (cardId) => {
-  axios.delete(`${API_ROOT}/cards/${cardId}`);
+  try {
+    axios.delete(`${API_ROOT}/cards/${cardId}`);
+  } catch (err) {
+    console.log(err);
+  }
 };
